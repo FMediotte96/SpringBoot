c6ae49bf0e3ce0a48acca0e4d6693306d4e3a8b7
Facundo, Mediotte <facumediotte@gmail.com>
2020-06-07 00:49:54 -0300

    SpringSecurity a nivel Base de datos

--- SpringBootMVC/src/main/java/com/facu/SecurityConfig.java
+++ SpringBootMVC/src/main/java/com/facu/SecurityConfig.java
@@ -1,12 +1,30 @@
 package com.facu;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.facu.service.user.UserService;
+
+/**
+ * Clase creada para implementar seguridad contra la base de datos con autenticación de usuario
+ * @author fmediotte
+ *
+ */
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity //Habilito la seguridad Web
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	private UserService userDetailsService;
+	
+	@Autowired
+	private BCryptPasswordEncoder bcrypt;
 	/**
 	 * Encriptador de contraseñas que me proporciona SpringBoot Security
 	 * @return
@@ -17,4 +35,23 @@ public class SecurityConfig {
 		return bCryptPasswordEncoder;
 	}
 
+	/**
+	 * Configuramos usuario y contraseña
+	 */
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bcrypt);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.httpBasic();
+	}
+	
+	
 }
--- SpringBootMVC/src/main/java/com/facu/repository/IUsuarioRepo.java
+++ SpringBootMVC/src/main/java/com/facu/repository/IUsuarioRepo.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.facu.model.Usuario;
 
 public interface IUsuarioRepo extends JpaRepository<Usuario, Integer> {
+	
+	Usuario findByNombre(String nombre); //Esto disparara una consulta que busque con where Nombre
 
 }
