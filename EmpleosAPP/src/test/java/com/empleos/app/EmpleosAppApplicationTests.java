be168908654d30fe2b70d67063a06226e18d6adc
Facundo Mediotte <facumediotte@gmail.com>
2021-01-30 14:57:09 -0300

    EmpleosApp
    Spring MVC Thymeleaf

---
+++ EmpleosAPP/src/test/java/com/empleos/app/EmpleosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empleos.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
