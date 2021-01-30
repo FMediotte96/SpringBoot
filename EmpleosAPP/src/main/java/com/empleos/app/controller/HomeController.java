b5e7f7735c761d108d2cc2b52ef03510cb304747
Facundo Mediotte <facumediotte@gmail.com>
2021-01-30 15:35:52 -0300

    Spring MVC
    Agregando tipo de dato List al Modelo y desplegarlo en una tabla html con thymeleaf

--- EmpleosAPP/src/main/java/com/empleos/app/controller/HomeController.java
+++ EmpleosAPP/src/main/java/com/empleos/app/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.empleos.app.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
@@ -8,8 +10,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.empleos.app.model.Vacante;
+
 @Controller
 public class HomeController {
+	
+	@GetMapping("/tabla")
+	public String mostrarTabla(Model model) {
+		List<Vacante> lista = getVacantes();
+		model.addAttribute("vacantes", lista);
+		
+		return "tabla";
+	}
+	
+	@GetMapping("/detalle")
+	public String mostrarDetalle(Model model) {
+		Vacante vacante = new Vacante();
+		vacante.setNombre("Ingeniero de comunicaciones");
+		vacante.setDescripcion("Se solicita ingeniero para dar soporte a intranet");
+		vacante.setFecha(new Date());
+		vacante.setSalario(97000.0);
+		
+		model.addAttribute("vacante", vacante);
+		
+		return "detalle";
+	}
 	
 	@GetMapping("/listado")
 	public String mostrarListado(Model model) {
@@ -42,6 +67,59 @@ public class HomeController {
 		model.addAttribute("vigente", vigente);
 		
 		return "home";
+	}
+	
+	/**
+	 * Método que regresa una lista de objetos de tipo Vacante
+	 * @return
+	 */
+	private List<Vacante> getVacantes() {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+		List<Vacante> lista = new LinkedList<Vacante>();
+		
+		try {
+			//Creamos la oferta de Trabajo 1
+			Vacante vacante1 = new Vacante();
+			vacante1.setId(1);
+			vacante1.setNombre("Ingeniero Civil");
+			vacante1.setDescripcion("Solicitamos Ing. Civil para diseñar puente peatonal");
+			vacante1.setFecha(sdf.parse("08-02-2021"));
+			vacante1.setSalario(125000.0);
+			
+			Vacante vacante2 = new Vacante();
+			vacante2.setId(2);
+			vacante2.setNombre("Contador Publico");
+			vacante2.setDescripcion("Empresa importante solicita contador con 5 años de experienca titulado");
+			vacante2.setFecha(sdf.parse("09-02-2021"));
+			vacante2.setSalario(80000.0);
+			
+			Vacante vacante3 = new Vacante();
+			vacante3.setId(3);
+			vacante3.setNombre("Ingeniero Eléctrico");
+			vacante3.setDescripcion("Empresa internacional solicita Ingeniero mecánico para mantenimiento de la instalación eléctrica");
+			vacante3.setFecha(sdf.parse("10-02-2021"));
+			vacante3.setSalario(105000.0);
+			
+			Vacante vacante4 = new Vacante();
+			vacante4.setId(4);
+			vacante4.setNombre("Diseñador Gráfico");
+			vacante4.setDescripcion("Solicitamos Diseñador Gráfico titulado para diseñar publicidad de la empresa");
+			vacante4.setFecha(sdf.parse("11-02-2019"));
+			vacante4.setSalario(80000.0);
+			
+			/**
+			 * Agregamos los 4 objetos de tipo Vacante a la lista
+			 */
+			lista.add(vacante1);
+			lista.add(vacante2);
+			lista.add(vacante3);
+			lista.add(vacante4);
+		} catch (ParseException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+		return lista;
 	}
 
 }
